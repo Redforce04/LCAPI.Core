c7a6a7a5579d2de1de5c22969602fc5fe8d229b9
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-12-04 12:36:29 -0600

    Add autosave mode.

---
+++ LethalAPI.Core/ModData/Enums/AutoSaveMode.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AutoSaveMode.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the LGPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace LethalAPI.Core.ModData.Enums;
+
+/// <summary>
+/// Indicates the method of auto-saving to use.
+/// </summary>
+public enum AutoSaveMode
+{
+    /// <summary>
+    /// Saves the data whenever an item is changed.
+    /// </summary>
+    ItemChanged,
+
+    /// <summary>
+    /// Saves will automatically occur whenever a game save takes place.
+    /// </summary>
+    GameSaved,
+
+    /// <summary>
+    /// No saves will occur unless triggered in the <see cref="SaveManager"/>.
+    /// </summary>
+    Manual,
+}
