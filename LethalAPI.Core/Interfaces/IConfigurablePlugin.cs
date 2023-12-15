f36d137d2e71562469c031d94c3af7ff11096d7f
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-12-15 14:07:19 -0600

    Convert Configs to separate interface.

---
+++ LethalAPI.Core/Interfaces/IConfigurablePlugin.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IConfigurablePlugin.cs" company="LethalAPI Modding Community">
+// Copyright (c) LethalAPI Modding Community. All rights reserved.
+// Licensed under the LGPL-3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace LethalAPI.Core.Interfaces;
+
+/// <summary>
+/// The wrapper interface for plugins utilizing Lethal Configs.
+/// </summary>
+/// <typeparam name="TConfig">The config type.</typeparam>
+public interface IConfigurablePlugin<TConfig>
+    where TConfig : IConfig
+{
+    /// <summary>
+    /// Gets or sets the config.
+    /// </summary>
+    /// <remarks>If utilizing the <see cref="IConfig"/> interface, this should be = new() / not null.</remarks>
+    public TConfig Config { get; set; }
+}
